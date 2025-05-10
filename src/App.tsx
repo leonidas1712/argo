cc7cc85ed307fc30627a426b6b2fcd722981fa09
Leonidas Raghav <leonidasr2000@gmail.com>
2025-05-10 22:54:11 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    const greeting = await invoke<string>("greet", { name });
+    setGreetMsg(greeting);
+  }
+
+  return (
+    <main className="container">
+      <h1>Welcome to Argo</h1>
+
+      <p>Enter your name below!</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+      <p>{greetMsg}</p>
+    </main>
+  );
+}
+
+export default App;
