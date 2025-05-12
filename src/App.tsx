b307932b15842f5e9784b17cfdcb6dbef64ebd3b
Leonidas Raghav <leonidasr2000@gmail.com>
2025-05-12 21:11:26 +0800

    replace styling and layout with mantine

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import '@mantine/core/styles.css';
 
-import { MantineProvider, Chip } from "@mantine/core";
-// import "./App.css";
+import { Container, Title, Text, TextInput, Button, Stack, Loader, Center } from "@mantine/core";
 
 function App() {
   const [result, setResult] = useState("");
@@ -18,31 +17,42 @@ function App() {
   }
 
   return (
-    <MantineProvider>
-      <main className="container">
-        <Chip defaultChecked>A chip</Chip>
-        <h1>Welcome to Argo</h1>
+      <Container size="sm" py="xl">
+          <Stack gap="md" align="center">
+            <Title order={1}>Welcome to Argo</Title>
+            <Text>Enter your name below!</Text>
 
-        <p>Enter your name below!</p>
+            <form
+              style={{ width: '80%' }}
+              onSubmit={(e) => {
+                e.preventDefault();
+                greet();
+              }}
+            >
+              <Stack gap="sm">
+                <TextInput
+                  placeholder="Enter a name..."
+                  value={name}
+                  onChange={(e) => setName(e.currentTarget.value)}
+                  size="sm"
+                />
+                <Button type="submit">
+                  Greet
+                </Button>
+              </Stack>
+            </form>
 
-        <form
-          className="row"
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
-
-        {loading ? <p>Loading...</p> : <p>Result: {result.length > 0 ? result : "Empty result."}</p>}
-      </main>
-    </MantineProvider>
+            {loading ? (
+              <Center>
+                <Loader size="sm" />
+              </Center>
+            ) : (
+              <Text>
+                Result: {result.length > 0 ? result : "Empty result."}
+              </Text>
+            )}
+          </Stack>
+      </Container>
   );
 }
 
