a8f455902d4e09ba9c51335d28062af49981c45e
Leonidas Raghav <leonidasr2000@gmail.com>
2025-05-15 10:41:06 +0800

    add comment

--- src/components/UserMessage.tsx
+++ src/components/UserMessage.tsx
@@ -1,11 +1,10 @@
 import { IconUser } from "@tabler/icons-react";
 import BaseMessage from "./BaseMessage";
-import ReactMarkdown from "react-markdown";
 
 function UserMessage({ content, time }: { content: string; time: string }) {
   return (
     <BaseMessage time={time} icon={IconUser} avatarColor="yellow">
-      {/* <ReactMarkdown>{content}</ReactMarkdown> */}
+      {/* Use p tag instead of ReactMarkdown so line breaks can be maintained */}
       <p style={{ whiteSpace: "pre-wrap" }}>{content}</p>
     </BaseMessage>
   );
