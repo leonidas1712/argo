c5b2a8047ecc4491f25183b70e6b65c77add11bf
Leonidas Raghav <leonidasr2000@gmail.com>
2025-05-15 10:46:47 +0800

    add gfm plugin

--- src/components/AIMessage.tsx
+++ src/components/AIMessage.tsx
@@ -1,11 +1,13 @@
 import { IconRobot } from "@tabler/icons-react";
 import BaseMessage from "./BaseMessage";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 function AIMessage({ content, time }: { content: string; time: string }) {
   return (
     <BaseMessage time={time} icon={IconRobot} avatarColor="blue">
-      <ReactMarkdown>{content}</ReactMarkdown>
+      {/* Gfm plugin gives tables, strikethroughs, etc */}
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
     </BaseMessage>
   );
 }
