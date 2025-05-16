f36d06a530eadf58e99e15bf37ba8163f3583915
Leonidas Raghav <leonidasr2000@gmail.com>
2025-05-16 13:57:17 +0800

    add comment

--- src/utils/formatTimestamp.ts
+++ src/utils/formatTimestamp.ts
@@ -1,3 +1,5 @@
+// Take ISO string and return user friendly formatted string
+// example output: 16 May 2025 at 1:50 pm
 export function formatTimestamp(isoString: string) {
   const date = new Date(isoString);
 
