dceacb2b7e0f7f7cb3fd111136ed3232904fe50c
Leonidas Raghav <leonidasr2000@gmail.com>
2025-05-20 14:29:42 +0800

    delete test thread migration

--- src-tauri/migrations/20250518150234_test_thread.sql
+++
@@ -1,8 +0,0 @@
--- Add migration script here
--- Insert test thread with test data
-INSERT OR IGNORE INTO threads (id, name, created_at) 
-VALUES (
-    'test',  -- Using a fixed UUID for test thread
-    'Test Thread',
-    '2025-05-18T10:45:49Z'
-);
